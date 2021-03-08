0e895f74af22ac6ded19769df03320b25e7e56b6
TDIM <49926777+TdimMax@users.noreply.github.com>
2021-03-09 01:26:24 +0200

    Update style.sh

--- style.sh
+++ style.sh
@@ -1,7 +1,8 @@
-termux-setup-storage
-pkg install wget
 cd $HOME 
+echo "[1] Getting Files Form Repo"
 wget https://raw.githubusercontent.com/TdimMax/fast-script/main/bash.bashrc
+echo "[2] Deleting Old Files"
 rm -rf $HOME/../usr/etc/motd $HOME/../usr/etc/bash.bashrc
+echo "[3] Installing Style"
 mv $HOME/bash.bashrc $HOME/../usr/etc/
-exit
+echo "[4] Please Restart Termux To See Changes !"
